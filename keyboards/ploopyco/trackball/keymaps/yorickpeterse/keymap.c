d5b3ce1e6a75fa80ce31af0054fc22b533ab5b88
Yorick Peterse <yorick@yorickpeterse.com>
2021-05-27 23:18:24 +0200

    Replace sniper button with Shift
    This makes it easier to select multiple lines of files, without having
    to use the keyboard.

--- keyboards/ploopyco/trackball/keymaps/yorickpeterse/keymap.c
+++ keyboards/ploopyco/trackball/keymaps/yorickpeterse/keymap.c
@@ -1,18 +1,13 @@
 // Custom keymap for my Ploopy trackball
+//
+// safe range starts at `PLOOPY_SAFE_RANGE` instead.
 
 #include QMK_KEYBOARD_H
-
-// safe range starts at `PLOOPY_SAFE_RANGE` instead.
 
 #define LCLICK KC_BTN1
 #define MCLICK KC_BTN3
 #define RCLICK KC_BTN2
-#define NEXT KC_BTN4
-#define PREV KC_BTN5
-
-enum custom_keycodes {
-    SNIPER_BUTTON = PLOOPY_SAFE_RANGE
-};
+#define SHIFT KC_LSHIFT
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
@@ -25,21 +20,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         // Top right
         MCLICK,
         // Bottom right
-        SNIPER_BUTTON
+        SHIFT
     )
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    switch(keycode) {
-        case SNIPER_BUTTON:
-            if(record->event.pressed) {
-                pmw_set_cpi(PLOOPY_DPI_SNIPER_VALUE);
-            } else {
-                pmw_set_cpi(PLOOPY_DPI_NORMAL_VALUE);
-            }
-
-            break;
-    }
-
     return true;
 }
