99024d0791c49eb47d8f8aa6d5772c347dfb1c76
Yorick Peterse <git@yorickpeterse.com>
2024-07-10 23:42:07 +0200

    Build firmware with Docker
    This should remove the need for installing the necessary
    cross-compilation toolchains in my main development container.

--- flash/skeletyl_handwired.fish
+++ flash/skeletyl_handwired.fish
@@ -1,2 +1,4 @@
 #!/usr/bin/env fish
-qmk flash -kb yorickpeterse/skeletyl/v2/splinky_3 -km default
+
+./util/docker_build.sh yorickpeterse/skeletyl/v2/splinky_3:default
+qmk flash -kb yorickpeterse/skeletyl/v2/splinky_3 -km default yorickpeterse_skeletyl_v2_splinky_3_default.uf2
