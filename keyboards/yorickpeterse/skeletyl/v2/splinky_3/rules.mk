6347c1f539692fa5b4686870e3b4393878ac28f7
Yorick Peterse <yorick@yorickpeterse.com>
2024-09-28 01:19:52 +0200

    Give combos another try and move some more keys

--- keyboards/yorickpeterse/skeletyl/v2/splinky_3/rules.mk
+++ keyboards/yorickpeterse/skeletyl/v2/splinky_3/rules.mk
@@ -10,6 +10,7 @@ NKRO_ENABLE = no            # Enable N-Key Rollover
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
 AUDIO_ENABLE = no           # Audio output
+COMBO_ENABLE = yes
 
 AUDIO_SUPPORTED = no       # Audio is not supported
 RGB_MATRIX_SUPPORTED = no  # RGB matrix is supported and enabled by default
