6cb6c8f3db5c9a5f3e1f1209682a5eb8d9bbd26e
Yorick Peterse <yorick@yorickpeterse.com>
2025-01-08 03:53:42 +0100

    Revert "Fix chaining of OSL layers"
    This reverts commit 972870c81abae900a396fcda433726c182119cc3.

--- quantum/action.c
+++ quantum/action.c
@@ -664,8 +664,6 @@ void process_action(keyrecord_t *record, action_t action) {
                             if (tap_count >= ONESHOT_TAP_TOGGLE) {
                                 reset_oneshot_layer();
                                 set_oneshot_layer(action.layer_tap.val, ONESHOT_TOGGLED);
-                            } else if (get_oneshot_layer() != action.layer_tap.val) {
-                                layer_off(action.layer_tap.val);
                             } else {
                                 clear_oneshot_layer_state(ONESHOT_PRESSED);
                             }
@@ -673,8 +671,6 @@ void process_action(keyrecord_t *record, action_t action) {
 #        else
                         if (event.pressed) {
                             set_oneshot_layer(action.layer_tap.val, ONESHOT_START);
-                        } else if (get_oneshot_layer() != action.layer_tap.val) {
-                            layer_off(action.layer_tap.val);
                         } else {
                             clear_oneshot_layer_state(ONESHOT_PRESSED);
                             if (tap_count > 1) {
