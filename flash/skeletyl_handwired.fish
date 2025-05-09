bdd6ad370ab9fe31b854566659dcf0f5a310cc48
Yorick Peterse <yorick@yorickpeterse.com>
2025-05-09 16:22:28 +0200

    Flash using just the qmk CLI

--- flash/skeletyl_handwired.fish
+++ flash/skeletyl_handwired.fish
@@ -1,4 +1,3 @@
 #!/usr/bin/env fish
 
-./util/docker_build.sh yorickpeterse/skeletyl/v2/splinky_3:default \
-    && qmk flash -kb yorickpeterse/skeletyl/v2/splinky_3 -km default yorickpeterse_skeletyl_v2_splinky_3_default.uf2
+qmk flash -kb yorickpeterse/skeletyl/v2/splinky_3 -km default
